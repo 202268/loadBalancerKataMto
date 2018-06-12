d41a6c8ea1e36a1fde33153f6faa20f216d6af6d
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-06-12 12:59:06 +0200

    refactoriazation

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -0,0 +1,24 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Matcher;
+
+public class Server {
+
+	private static final double MAXIMUM_LOAD = 100.0d;
+	public double currentLoadPercentage;
+	public int capacity;
+
+	public boolean contains(Vm theVm) {
+		return true;
+	}
+
+	public Server(int capacity) {
+		super();
+		this.capacity = capacity;
+	}
+
+	public void addVm(Vm vm) {
+		currentLoadPercentage = (double)vm.size / (double)capacity * MAXIMUM_LOAD;		
+	}
+
+}
---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -0,0 +1,39 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+
+	private static final double EPSILON = 0.01d;
+	private double expectedPercentage;
+
+	public CurrentLoadPercentageMatcher(double expectedPercentage) {
+		this.expectedPercentage = expectedPercentage;
+
+	}
+
+	public void describeTo(Description description) {
+		description.appendText("a server with load percentage of").appendValue(expectedPercentage);
+	}
+
+	@Override
+	protected void describeMismatchSafely(Server item, Description description) {
+		description.appendText("a server with load percentage of").appendValue(item.currentLoadPercentage);
+	}
+
+	@Override
+	protected boolean matchesSafely(Server server) {
+		return doublesAreEqual(expectedPercentage, server.currentLoadPercentage);
+	}
+
+	private boolean doublesAreEqual(double d1, double d2) {
+		return d1 == d2 || Math.abs(d1 - d2) < EPSILON;
+	}
+
+	public static CurrentLoadPercentageMatcher hasCurrentLoadOf(double expectedPercentage) {
+		return new CurrentLoadPercentageMatcher(expectedPercentage);
+	}
+
+}
