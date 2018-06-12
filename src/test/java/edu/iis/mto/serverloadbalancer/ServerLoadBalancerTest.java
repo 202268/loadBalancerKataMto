6af56c8dba2dcd3d052cb78c494e320bf87c5bdb
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-06-12 12:29:11 +0200

    balancing zero vms implemented

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,12 +4,39 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
 	@Test
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
+	}
+	@Test
+	public void balancingServerWithNoVms_serverStaysEmpty() {
+		Server theServer=a(server().withCapacity(1));
+		balancing(aServerListWith(theServer), anEmptyVmsList());
+		
+		assertThat(theServer, hasCurrentLoadOf(0.0d));
+	}
+	private Matcher<? super Server> hasCurrentLoadOf(double expectedPercentage) {
+		return new CurrentLoadPercentageMatcher(expectedPercentage);
+	}
+	private void balancing(Server[] aServerListWith, Vm[] anEmptyVmsList) {
+		new ServerLoadBalancer().balance(aServerListWith, anEmptyVmsList);
+	}
+	private Vm[] anEmptyVmsList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private Server a(ServerBuilder builder) {
+		return builder.build();
+	}
+	private Server[] aServerListWith(Server...servers) {
+		return servers;
+	}
+	private ServerBuilder server() {
+		return new ServerBuilder();
 	}
 
 
