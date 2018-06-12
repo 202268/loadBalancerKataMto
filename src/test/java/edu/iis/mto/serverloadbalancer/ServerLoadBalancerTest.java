54c599e908e89d4106fedc2d7143e281484606d0
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-06-12 12:45:19 +0200

    filling server with vm implemented

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,6 +1,5 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasCurrentLoadOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,28 +13,51 @@ public class ServerLoadBalancerTest {
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
+
 	@Test
 	public void balancingServerWithNoVms_serverStaysEmpty() {
-		Server theServer=a(server().withCapacity(1));
+		Server theServer = a(server().withCapacity(1));
 		balancing(aServerListWith(theServer), anEmptyVmsList());
-		
+
 		assertThat(theServer, hasCurrentLoadOf(0.0d));
 	}
-	
+
+	@Test
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsServerWithTheVm() {
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(vm().ofSize(1));
+		balancing(aServerListWith(theServer), aVmsListWith(theVm));
+
+		assertThat(theServer, hasCurrentLoadOf(100.0d));
+		assertThat("server should contain the vm", theServer.contains(theVm));
+	}
+
+	private Vm a(VmBuilder builder) {
+		return builder.build();
+	}
+
+	private Vm[] aVmsListWith(Vm... vms) {
+		return vms;
+	}
+
+	private VmBuilder vm() {
+		return new VmBuilder();
+	}
+
 	private void balancing(Server[] aServerListWith, Vm[] anEmptyVmsList) {
 		new ServerLoadBalancer().balance(aServerListWith, anEmptyVmsList);
 	}
+
 	private Vm[] anEmptyVmsList() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Vm[0];
 	}
+
 	private Server a(ServerBuilder builder) {
 		return builder.build();
 	}
-	private Server[] aServerListWith(Server...servers) {
+
+	private Server[] aServerListWith(Server... servers) {
 		return servers;
 	}
-	
-
 
 }
